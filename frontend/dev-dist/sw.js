45ae93576a6d1dcd2985759abb0f2de7e588fc02
JaysonCV30 <jayson.chico@uabc.edu.mx>
2026-05-19 16:31:49 -0700

    Integration complete: Added PWA features and unified Docker configs

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-1e60b829'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.e5khessk8eo"
+    "revision": "0.l1rrbdv7ok"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
